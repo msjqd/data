264dbeb41aa5faa6f1b07ea9b01f6243aea62763
miao songjie <msj_2008@163.com>
2016-08-22 08:08:45 +0800

    $json.get
    freecodecamp测试数据

---
+++ data01.js
@@ -0,0 +1,17 @@
+[{
+"author":"Dracula"
+"content":"Listen to them. Children of the night. What music they make."
+},{
+"author":"James Branch Cabell"
+"content":"The optimist proclaims that we live in the best of all possible worlds, and the pessimist fears this is true."
+},{
+"author":"Apocalypse Now"
+"content":"I love the smell of napalm in the morning."
+},{
+"author":"Clarence Darrow"
+"content":"I have never killed anyone, but I have read some obituary notices with great satisfaction."
+},{
+"author":"Definition of 'happiness' by J"
+"content":"The full use of your powers along lines of excellence."
+}
+]
